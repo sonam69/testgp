77574b07c3eb374da7a51f08492ba4b1720ca70f
Manos Tzelepis <tzeldev@gmail.com>
2023-02-28 10:59:42 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import BarChart from "./components/BarChart";
-import LineChart from "./components/LineChart";
-import PieChart from "./components/PieChart";
-import { UserData } from "./Data";
+import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
-
   return (
     <div className="App">
-      <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
-      </div>
-      <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
-      </div>
-      <div style={{ width: 700 }}>
-        <PieChart chartData={userData} />
-      </div>
+      hello
+      <Routes> 
+            <Route path="/" element={<Home/> } /> 
+            <Route path="/about" element={<About/> } /> 
+       </Routes> 
     </div>
   );
 }
