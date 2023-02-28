e5d609c76757d78c1eabbae373e7143c4544098e
Manos Tzelepis <tzeldev@gmail.com>
2023-02-28 11:05:06 +0200

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/testgp">
     <App />
     </BrowserRouter>
   </React.StrictMode>,
